f68246a4035933692934d8b6f59a73a7b786a6cd
Avleen Dhillon <77461193+adhillon0817@users.noreply.github.com>
2022-08-04 21:22:43 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,46 @@
 
 // TODO: Create an array of questions for user input
 const questions = [];
+return questions.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What would you like to title your ReadMe?',
+        },
 
+        {
+            type: 'input',
+            name: 'Table of Contents',
+            message: "What all goes in your table of content?",
+        },
+        {
+            type: 'input',
+            name: 'Description',
+            message: "How would you describe your work?",
+        },
+        {
+            type: 'input',
+            name: 'Installation',
+            message: "What installations did you have to go through?",
+        },
+        {
+            type: 'input',
+            name: 'Usage',
+            message: "How do you use the application?",
+        },
+        {
+            type: 'input',
+            name: 'Contributing',
+            message: "What are the contributions?",
+        },
+        {
+            type: 'input',
+            name: 'Tests',
+            message: "What test did you run in order to get this ourcome?",
+        },
+        
+    ])
+}
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
